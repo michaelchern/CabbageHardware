34f40032a7e58884762a3d653a00177e77311f35
GraphZou <906520989@qq.com>
2025-07-22 02:26:18 +0800

    fix

--- Src/Hardware/GlobalContext.cpp
+++ Src/Hardware/GlobalContext.cpp
@@ -38,16 +38,16 @@ HardwareContext::HardwareContext()
     chooseMainDevice();
 
     // demo of mutilple devices
-    if (hardwareUtils.size()>1)
-    {
-        ResourceManager::ImageHardwareWrap a = hardwareUtils[0].resourceManager.createImage(ktm::uvec2(800, 800), VK_FORMAT_R8G8B8A8_UINT, 4, VK_IMAGE_USAGE_TRANSFER_SRC_BIT | VK_IMAGE_USAGE_TRANSFER_DST_BIT);
-        ResourceManager::ImageHardwareWrap b = hardwareUtils[1].resourceManager.createImage(ktm::uvec2(800, 800), VK_FORMAT_R8G8B8A8_UINT, 4, VK_IMAGE_USAGE_TRANSFER_SRC_BIT | VK_IMAGE_USAGE_TRANSFER_DST_BIT);
+    //if (hardwareUtils.size()>1)
+    //{
+    //    ResourceManager::ImageHardwareWrap a = hardwareUtils[0].resourceManager.createImage(ktm::uvec2(800, 800), VK_FORMAT_R8G8B8A8_UINT, 4, VK_IMAGE_USAGE_TRANSFER_SRC_BIT | VK_IMAGE_USAGE_TRANSFER_DST_BIT);
+    //    ResourceManager::ImageHardwareWrap b = hardwareUtils[1].resourceManager.createImage(ktm::uvec2(800, 800), VK_FORMAT_R8G8B8A8_UINT, 4, VK_IMAGE_USAGE_TRANSFER_SRC_BIT | VK_IMAGE_USAGE_TRANSFER_DST_BIT);
 
-        hardwareUtils[0].resourceManager.copyImageMemory(a, b);
-        hardwareUtils[0].resourceManager.copyImageMemory(b, a);
-        hardwareUtils[1].resourceManager.copyImageMemory(a, b);
-        hardwareUtils[1].resourceManager.copyImageMemory(b, a);
-    }
+    //    hardwareUtils[0].resourceManager.copyImageMemory(a, b);
+    //    hardwareUtils[0].resourceManager.copyImageMemory(b, a);
+    //    hardwareUtils[1].resourceManager.copyImageMemory(a, b);
+    //    hardwareUtils[1].resourceManager.copyImageMemory(b, a);
+    //}
     // demo of mutilple devices
 }
 
