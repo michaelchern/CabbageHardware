64eab5a1a6cb18ae6d15073029022fa9ad75be07
JOPLOPOL <95957645+JOPLOPOL@users.noreply.github.com>
2025-07-21 16:17:59 +0800

    删掉无用代码

--- Src/Resource/HardwarePushConstant.cpp
+++ Src/Resource/HardwarePushConstant.cpp
@@ -36,8 +36,6 @@ HardwarePushConstant::~HardwarePushConstant()
 
 HardwarePushConstant &HardwarePushConstant::operator=(const HardwarePushConstant &other)
 {
-    if (this == &other) return *this;
-
     if (pushConstantGlobalPool[*this->pushConstantID].size == pushConstantGlobalPool[*other.pushConstantID].size && pushConstantGlobalPool[*this->pushConstantID].data != nullptr && pushConstantGlobalPool[*other.pushConstantID].data != nullptr)
     {
         memcpy(pushConstantGlobalPool[*this->pushConstantID].data, pushConstantGlobalPool[*other.pushConstantID].data, pushConstantGlobalPool[*other.pushConstantID].size);
