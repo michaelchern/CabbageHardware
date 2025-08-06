96510ab5a98b73be96e4d5b16cd30b520b69e5e6
GraphZou <906520989@qq.com>
2025-08-06 19:35:03 +0800

    fix绑定

--- Examples/frag.glsl
+++ Examples/frag.glsl
@@ -24,7 +24,7 @@ layout(location = 3) in vec3 inColor;
 
 layout(location = 0) out vec4 outColor;
 
-layout(set = 0, binding = 1) uniform sampler2D textures[];
+layout(set = 1, binding = 0) uniform sampler2D textures[];
 
 // ----------------------------------------------------------------------------
 float DistributionGGX(vec3 N, vec3 H, float roughness)
