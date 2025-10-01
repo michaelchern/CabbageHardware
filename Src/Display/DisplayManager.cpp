29a6d364bff72d91287b19dc840f382777a053c4
GraphZou <906520989@qq.com>
2025-10-01 18:16:57 +0800

    测试零拷贝

--- Src/Display/DisplayManager.cpp
+++ Src/Display/DisplayManager.cpp
@@ -289,14 +289,8 @@ bool DisplayManager::displayFrame(void *displaySurface, HardwareImage displayIma
 
             initDisplayManager(displaySurface);
 
-			this->displayImage = displayDevice->resourceManager.createImage(imageGlobalPool[*displayImage.imageID].imageSize, imageGlobalPool[*displayImage.imageID].imageFormat,
-                                                                            imageGlobalPool[*displayImage.imageID].pixelSize, imageGlobalPool[*displayImage.imageID].imageUsage);
-
-            
-            VkDeviceSize imageSizeBytes = this->displayImage.imageSize.x * this->displayImage.imageSize.y * this->displayImage.pixelSize;
-
-            srcStaging = globalHardwareContext.mainDevice->resourceManager.createBuffer(imageSizeBytes, VK_BUFFER_USAGE_TRANSFER_DST_BIT);
-            dstStaging = displayDevice->resourceManager.createBuffer(imageSizeBytes, VK_BUFFER_USAGE_TRANSFER_DST_BIT);
+            // 首次初始化时，将 importedImageID 置空，以强制在下面进行首次导入
+            importedImageID = nullptr;
         }
 
 		VkSurfaceCapabilitiesKHR capabilities;
@@ -326,8 +320,37 @@ bool DisplayManager::displayFrame(void *displaySurface, HardwareImage displayIma
 
 		if (result == VK_SUCCESS || result == VK_SUBOPTIMAL_KHR)
         {
-            // auto start_time_ = std::chrono::high_resolution_clock::now();
-            displayDevice->resourceManager.copyImageMemory(imageGlobalPool[*displayImage.imageID], this->displayImage, &srcStaging, &dstStaging);
+            ResourceManager::ImageHardwareWrap &sourceImage = imageGlobalPool[*displayImage.imageID];
+
+            // 检查主渲染设备和显示设备是否为同一个 GPU
+            if (globalHardwareContext.mainDevice == displayDevice)
+            {
+                // 如果是同一个设备，直接使用源图像进行显示，无需任何拷贝或导入
+                this->displayImage = sourceImage;
+            }
+            else
+            {
+                // 如果是不同设备，执行零拷贝导入/导出
+                // 检查当前帧的图像是否是已经导入的图像
+                if (importedImageID == nullptr || *importedImageID != *displayImage.imageID)
+                {
+                    // 如果不是，或者从未导入过，则执行导入操作
+                    // 1. 从源设备导出内存句柄
+                    ResourceManager::ExternalMemoryHandle memHandle = globalHardwareContext.mainDevice->resourceManager.exportImageMemory(sourceImage);
+
+                    // 2. 在显示设备上导入内存
+                    //    销毁旧的导入图像（如果存在）
+                    if (this->displayImage.imageHandle != VK_NULL_HANDLE)
+                    {
+                        displayDevice->resourceManager.destroyImage(this->displayImage);
+                    }
+                    this->displayImage = displayDevice->resourceManager.importImageMemory(memHandle, sourceImage);
+
+                    // 3. 更新已导入的图像ID
+                    importedImageID = displayImage.imageID;
+                }
+                // 如果图像ID相同，则 this->displayImage 已经是正确的导入图像，无需任何操作
+            }
 
             auto runCommand = [&](const VkCommandBuffer &commandBuffer) {
                 //// Transition displayImage to VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL
